cf54092702a831d344ec6b17f9a9109bd1032713
Joshua Aquino <email@joshuaaquino.xyz>
2023-10-07 19:41:22 -0400

    add app icon and update intents in manifest

--- app/src/main/java/com/example/justweb/MainActivity.java
+++ app/src/main/java/com/example/justweb/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -17,10 +18,38 @@ public class MainActivity extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://joshuaaquino.xyz");
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        Intent intent=getIntent();
+        if(intent!=null) {
+            String action=intent.getAction();
+            String type=intent.getType();
+            if(Intent.ACTION_SEND.equals(action) && type!=null) {
+                webView.loadUrl(handleTextData(intent));
+            } else if (Intent.ACTION_VIEW.equals(action)) {
+                // Handle the URL passed via ACTION_VIEW
+                String url = intent.getDataString();
+                if (url != null) {
+                    webView.loadUrl(url);
+                } else {
+                    webView.loadUrl("https://joshuaaquino.xyz/blog/11");
+                }
+            } else {
+                webView.loadUrl("https://joshuaaquino.xyz/blog/12");
+            }
+        } else {
+            webView.loadUrl("https://joshuaaquino.xyz/blog/13");
+        }
+    }
+
+    private String handleTextData(Intent intent) {
+        String textdata=intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(textdata!=null) {
+            return textdata;
+        }
+        return "https://joshuaaquino.xyz";
     }
 
     @Override
